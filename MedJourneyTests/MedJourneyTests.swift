a9edf9d0902633a3dab0b7dd32df1d039256d227
Wahyu Herdianto <wahyuherdianto12@gmail.com>
2026-05-19 10:59:09 +0700

    Initial Commit

---
+++ MedJourneyTests/MedJourneyTests.swift
@@ -0,0 +1,16 @@
+//
+//  MedJourneyTests.swift
+//  MedJourneyTests
+//
+//  Created by user on 19/05/26.
+//
+
+import Testing
+
+struct MedJourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
